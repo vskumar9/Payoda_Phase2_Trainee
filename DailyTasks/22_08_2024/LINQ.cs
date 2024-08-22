024b4b87025ac35429aeedab75c7e4c0513fbc7f
vskumar9 <vaddisanjeevkumar9676@gmail.com>
2024-08-22 20:18:48 +0530

    22_08_2024_Task_Completed

--- DailyTasks/22_08_2024/LINQ.cs
+++ DailyTasks/22_08_2024/LINQ.cs
@@ -34,11 +34,11 @@ public class Program
 
 
 		var result_1 = enrollments
-		               .GroupBy(e => e.StudentId)
-		               .Where(g => g.Count() >= 2)
-		               .Select(g => students.FirstOrDefault(s => s.StudentId == g.Key).Name);
-        
-        Console.WriteLine();
+					   .GroupBy(e => e.StudentId)
+					   .Where(g => g.Count() >= 2)
+					   .Select(g => students.FirstOrDefault(s => s.StudentId == g.Key).Name);
+
+		Console.WriteLine();
 		Console.WriteLine("List of students enrolled in at least three courses:");
 		foreach (var s in result_1)
 		{
@@ -47,23 +47,25 @@ public class Program
 
 
 		var result_2 = enrollments
-		               .GroupBy(e => e.StudentId)
-		               .Select(g => new {
-		                   StudentName = students.FirstOrDefault(s => s.StudentId == g.Key).Name,
-		                   CourseCount = g.Count()
-		               })
-		               .OrderBy(e => e.CourseCount)
-		               .GroupBy(e => e.CourseCount)
-		               .Select(g => new {
-		                   CourseCount = g.Key,
-		                   StudentNames = string.Join(", ", g.Select(x => x.StudentName))
-		               });
-		               
-        Console.WriteLine();
+					   .GroupBy(e => e.StudentId)
+					   .Select(g => new
+					   {
+						   StudentName = students.FirstOrDefault(s => s.StudentId == g.Key).Name,
+						   CourseCount = g.Count()
+					   })
+					   .OrderBy(e => e.CourseCount)
+					   .GroupBy(e => e.CourseCount)
+					   .Select(g => new
+					   {
+						   CourseCount = g.Key,
+						   StudentNames = string.Join(", ", g.Select(x => x.StudentName))
+					   });
+
+		Console.WriteLine();
 		Console.WriteLine("Group students by the number of courses they are enrolled in:");
 		foreach (var group in result_2)
 		{
-			if(group.CourseCount == 1)
+			if (group.CourseCount == 1)
 				Console.WriteLine($"{group.CourseCount} Course: {group.StudentNames}");
 			else
 				Console.WriteLine($"{group.CourseCount} Courses: {group.StudentNames}");
@@ -71,26 +73,30 @@ public class Program
 		}
 
 		var result_3 = enrollments
-		               .Join(students,
-		               e => e.StudentId,
-		               s => s.StudentId, 
-		               (e, s) => new {
-		                   Enrollment = e, Student = s
-		               })
-		               .Join(courses,
-		               e => e.Enrollment.CourseId,
-		               c => c.CourseId, 
-		               (e, c) => new {
-		                   StudentName = e.Student.Name, 
-		                   CourseName = c.Title
-		               })
-		               .GroupBy(e => e.CourseName)
-		               .Where(e => e.Count() > 2)
-		               .Select(g => new {
-		                   CourseName = g.Key,
-		                   StudentNames = string.Join(", ", g.Select(x => x.StudentName))
-		               });
-		               
+					   .Join(students,
+					   e => e.StudentId,
+					   s => s.StudentId,
+					   (e, s) => new
+					   {
+						   Enrollment = e,
+						   Student = s
+					   })
+					   .Join(courses,
+					   e => e.Enrollment.CourseId,
+					   c => c.CourseId,
+					   (e, c) => new
+					   {
+						   StudentName = e.Student.Name,
+						   CourseName = c.Title
+					   })
+					   .GroupBy(e => e.CourseName)
+					   .Where(e => e.Count() > 2)
+					   .Select(g => new
+					   {
+						   CourseName = g.Key,
+						   StudentNames = string.Join(", ", g.Select(x => x.StudentName))
+					   });
+
 		Console.WriteLine();
 		Console.WriteLine("Course with students enrolled in more than one course:");
 
@@ -98,50 +104,52 @@ public class Program
 		{
 			Console.WriteLine($"Course: {group.CourseName}, Students: {group.StudentNames}");
 		}
-		
+
 		var result_4 = enrollments
-		               .Join(courses,
-		               e => e.CourseId,
-		               c => c.CourseId,
-		               (e, c) => new {
-		                  studentId = e.StudentId,
-		                  courseName = c.Title
-		               })
-		               .GroupBy(e => e.courseName)
-		               .OrderByDescending(e => e.Count())
-		               .Select(g => new {
-		                   courseName = g.Key,
-		                   studentCount = g.Count()
-		               });
-		               
-		 Console.WriteLine();
-		 Console.WriteLine("Course sorted by the number of students enrolled:");
-		 foreach(var course in result_4)
-		 {
-		     Console.WriteLine($"{course.courseName} ({course.studentCount} students)");
-		 }
-		
+					   .Join(courses,
+					   e => e.CourseId,
+					   c => c.CourseId,
+					   (e, c) => new
+					   {
+						   studentId = e.StudentId,
+						   courseName = c.Title
+					   })
+					   .GroupBy(e => e.courseName)
+					   .OrderByDescending(e => e.Count())
+					   .Select(g => new
+					   {
+						   courseName = g.Key,
+						   studentCount = g.Count()
+					   });
+
+		Console.WriteLine();
+		Console.WriteLine("Course sorted by the number of students enrolled:");
+		foreach (var course in result_4)
+		{
+			Console.WriteLine($"{course.courseName} ({course.studentCount} students)");
+		}
+
 
 	}
 }
 
 public class Student
 {
-	public int StudentId{set; get;}
-	public string Name{set; get;}
+	public int StudentId { set; get; }
+	public string Name { set; get; }
 }
 
 
 public class Course
 {
-	public int CourseId{set; get;}
-	public string Title{set; get;}
+	public int CourseId { set; get; }
+	public string Title { set; get; }
 }
 
 public class Enrollment
 {
-	public int StudentId{set; get;}
-	public int CourseId{set; get;}
+	public int StudentId { set; get; }
+	public int CourseId { set; get; }
 }
 
 
