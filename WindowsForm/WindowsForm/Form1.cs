89cbe5c08868cf2a5999a76d2b86b12f543d0d8d
V_SANJEEV_KUMAR <72122950+vskumar9@users.noreply.github.com>
2024-08-23 16:47:21 +0530

    XML File (Console & Windows Application)

---
+++ WindowsForm/WindowsForm/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlReader xmlread = XmlReader.Create(@"C:\Users\sanjeevkumar.v\Desktop\Training\Payoda_Phase2_Trainee\DailyTasks\23_08_2024\XmlTask\XmlTask\bin\Debug\net8.0\Courses.xml");
+            while (xmlread.Read())
+            {
+                switch (xmlread.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        listBox1.Items.Add("<" + xmlread.Name + ">");
+                        break;
+                    case XmlNodeType.Text:
+                        listBox1.Items.Add(xmlread.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        listBox1.Items.Add("</Course>");
+                        break;
+                }
+            }
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            XDocument doc = XDocument.Load(@"C:\Users\sanjeevkumar.v\Desktop\Training\Payoda_Phase2_Trainee\DailyTasks\23_08_2024\XmlTask\XmlTask\bin\Debug\net8.0\Courses.xml");
+            //linq query
+            var elets = from ele in doc.Descendants("Course")
+                        where Convert.ToInt32(ele.Element("DurationInMonths").Value) > 1
+                        select ele;
+            foreach (var el in elets)
+            {
+                listBox2.Items.Add(el.Name + ":" + el.Value);
+            }
+        }
+    }
+}
---
+++ XmlTask/XmlTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+
+namespace XmlTask
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Course[] courses = new Course[3];
+            courses[0] = new Course(11, "DotNetCore", 3);
+            courses[1] = new Course(12, "Angular", 1);
+            courses[2] = new Course(13, "Data Analytics", 2);
+            using (XmlWriter writer = XmlWriter.Create("Courses.xml"))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Courses");
+                foreach (Course course in courses)
+                {
+                    writer.WriteStartElement("Course");
+                    writer.WriteElementString("CourseID", course.Cid.ToString());
+                    writer.WriteElementString("CourseName", course.Cname.ToString());
+                    writer.WriteElementString("DurationInMonths", course.Cduration.ToString());
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+
+        }
+    }
+
+}
