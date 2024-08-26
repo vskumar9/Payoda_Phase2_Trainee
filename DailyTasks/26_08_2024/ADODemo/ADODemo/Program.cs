96c7c0d6ca5bd5e2ec837489f710775511725b70
vskumar9 <vaddisanjeevkumar9676@gmail.com>
2024-08-26 16:45:31 +0530

    ADO Task

---
+++ DailyTasks/26_08_2024/ADODemo/ADODemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Data.SqlClient;
+
+internal class Program
+{
+    private static SqlConnection conn;
+
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, World!");
+        Create();
+        Insert();
+        Fetch();
+    }
+
+    public static void Create()
+    {
+        using(conn = new SqlConnection("data source=PTSQLTESTDB01;database=KUMAR;integrated security=true;"))
+        {
+            SqlCommand cmd = new SqlCommand("create table sample_ADO(id int primary key, name varchar(50))", conn);
+            conn.Open();
+            int val = cmd.ExecuteNonQuery();
+
+            //if (val > 0)
+            //{
+                Console.WriteLine("Table Create successfully");
+                Console.WriteLine("Query successfully executed.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Table not created.");
+            //}
+        }
+    }
+
+    public static void Insert()
+    {
+        using (conn = new SqlConnection("data source=PTSQLTESTDB01;database=KUMAR;integrated security=true;"))
+        {
+
+            SqlCommand cmd = new SqlCommand("insert into sample_ADO values(1, 'Sanjeev')", conn);
+            conn.Open();
+            int val = cmd.ExecuteNonQuery();
+
+            if (val > 0)
+            {
+                Console.WriteLine("Inserted data.");
+            }
+            else
+            {
+                Console.WriteLine("Not Inserted.");
+            }
+        }
+    }
+
+    public static void Fetch()
+    {
+        using (conn = new SqlConnection("data source=PTSQLTESTDB01;database=KUMAR;integrated security=true;"))
+        {
+            SqlCommand cmd = new SqlCommand("select * from sample_ADO", conn);
+            conn.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            while(sdr.Read())
+            {
+                Console.WriteLine(sdr[0].ToString() + " " + sdr[1].ToString());
+            }
+        }
+    }
+}
