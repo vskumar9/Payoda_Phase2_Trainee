bb6d1a7ea4716c0394111b8afc6ff0610d147aba
vskumar9 <vaddisanjeevkumar9676@gmail.com>
2024-09-03 18:19:43 +0530

    Completed Identity Application

---
+++ DailyTasks/03_09_2024/SampleIdentityDemo/SampleIdentityDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SampleIdentityDemo.Models;
+using System.Diagnostics;
+
+namespace SampleIdentityDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[Authorize]
+        //[Route("EmployeeList")]//Url routing page
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DailyTasks/03_09_2024/SampleIdentityDemo/SampleIdentityDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SampleIdentityDemo.Models;
+
+namespace SampleIdentityDemo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
