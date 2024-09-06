5dc6daa1acd1652d3ab33ad3c4ce09e8c64c569d
vskumar9 <vaddisanjeevkumar9676@gmail.com>
2024-09-06 14:08:51 +0530

    Upload Module-1 assessment code

---
+++ Assessment/Module-1/InsuranceCalculationWorkwithPolymorphism/InsuranceCalculationWorkwithPolymorphism/LifeInsurance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceCalculationWorkwithPolymorphism
+{
+    internal class LifeInsurance : Insurance
+    {
+        int policyTerm;
+        float benefitPercent;
+
+        public int PolicyTerm
+        {
+            set
+            {
+                this.policyTerm = value;
+            }
+            get
+            {
+                return this.policyTerm;
+            }
+        }
+
+        public float BenefitPercent
+        {
+            set
+            {
+                this.benefitPercent = value;
+            }
+            get
+            {
+                return this.benefitPercent;
+            }
+        }
+
+        public double calculatePremium()
+        {
+            return (AmountCovered - ((AmountCovered * BenefitPercent) / 100)) / PolicyTerm;
+        }
+    }
+}
