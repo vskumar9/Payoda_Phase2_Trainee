e62ffa8e45c88e5a79ba53b46277fccd8422b132
V_SANJEEV_KUMAR <72122950+vskumar9@users.noreply.github.com>
2024-09-06 14:13:25 +0530

    Delete Assessment/SocialMediaPlatform/SocialMediaPlatform directory

--- Assessment/SocialMediaPlatform/SocialMediaPlatform/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SocialMediaPlatform.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
