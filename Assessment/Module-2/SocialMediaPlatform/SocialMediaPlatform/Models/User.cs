6e52147fdaa750054600e3740361fec2503b4bbf
vskumar9 <vaddisanjeevkumar9676@gmail.com>
2024-09-06 14:11:44 +0530

    Upload Module-2 assessment code

---
+++ Assessment/Module-2/SocialMediaPlatform/SocialMediaPlatform/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaPlatform.Models
+{
+    public class User
+    {
+        [Key]
+        public int uId { get; set; }
+        public string? Username {  get; set; }
+
+        public string? Email { get; set; }
+
+        public ICollection<Post>? Posts { get; set; }
+
+    }
+}
